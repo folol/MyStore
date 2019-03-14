7841445a26711e4bfe6143cb5ae46e923a3f17be
Devansh <devansh@trainman.in>
2019-03-15 00:29:26 +0530

    using core data- r/w

---
+++ MyStore/DeviceDetailViewControllerUIViewController.swift
@@ -0,0 +1,89 @@
+//
+//  DeviceDetailViewControllerUIViewController.swift
+//  MyStore
+//
+//  Created by Devansh on 14/03/19.
+//  Copyright © 2019 Devansh. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DeviceDetailViewControllerUIViewController: UIViewController {
+
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var versionTF: UITextField!
+    @IBOutlet weak var companyTF: UITextField!
+    
+    @IBAction func cancelButtonPress(_ sender: Any) {
+         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func saveButtonPress(_ sender: Any) {
+        
+        if let nameStr = nameTF.text {
+            if let versionStr = versionTF.text {
+                if let companyStr = companyTF.text {
+                    guard let appDelegate =
+                        UIApplication.shared.delegate as? AppDelegate else {
+                            return
+                    }
+                    let managedContext =
+                        appDelegate.persistentContainer.viewContext
+                    let entity =
+                        NSEntityDescription.entity(forEntityName: "Device",
+                                                   in: managedContext)!
+                    let person = NSManagedObject(entity: entity,
+                                                 insertInto: managedContext)
+                    person.setValue(nameStr, forKeyPath: "name")
+                    person.setValue(versionStr, forKey: "version")
+                    person.setValue(companyStr, forKey: "company")
+                    do {
+                        try managedContext.save()
+                        self.navigationController?.popViewController(animated: true)
+                    } catch let error as NSError {
+                        print("Could not save. \(error), \(error.userInfo)")
+                    }
+                }
+                else{
+                    showAlert(title: "Error", message: "Enter country")
+                    return
+                }
+            }
+            else{
+                showAlert(title: "Error", message: "Enter version")
+                return
+            }
+        }
+        else{
+            showAlert(title: "Error", message: "Enter name")
+            return
+        }
+        
+    }
+    
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyStore/ViewController.swift
+++ MyStore/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var devicesArr : [NSManagedObject]?
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return devicesArr != nil ? devicesArr!.count : 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellView = tableView.dequeueReusableCell(withIdentifier: "LIST_IDENTIFIER", for: indexPath)
+    
+        cellView.textLabel?.text = devicesArr != nil && indexPath.row < devicesArr!.count
+            ? (devicesArr![indexPath.row] as? Device)?.name : "No data available"
+        
+        return cellView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Device")
+        do {
+            devicesArr = try managedContext.fetch(fetchRequest)
+            self.tableView.reloadData()
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
 }
 
